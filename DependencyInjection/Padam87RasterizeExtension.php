ea0fe48aa553d9f5e608692640a2cfff4f477808
Adam Prager <prager.adam87@gmail.com>
2019-01-16 23:37:49 +0100

    Changed service config (BC break)

--- DependencyInjection/Padam87RasterizeExtension.php
+++ DependencyInjection/Padam87RasterizeExtension.php
@@ -22,8 +22,8 @@ class Padam87RasterizeExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.xml');
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yaml');
 
         $container->setParameter('padam87_rasterize.config', $config);
     }
