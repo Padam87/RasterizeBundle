445f736c3639c9e68c260b81fe1759d94fcb3219
Tom Borealis <tom@lampbristol.com>
2020-09-28 17:20:46 +0100

    Use correct TreeBuilder root node method for symfony/config version

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -14,8 +14,11 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder('padam87_rasterize');
+        $rootNode = method_exists($treeBuilder, 'getRootNode')
+            ? $treeBuilder->getRootNode()
+            : $treeBuilder->root('padam87_rasterize');
 
-        $treeBuilder->getRootNode()
+        $rootNode
             ->children()
                 ->arrayNode('script')
                     ->addDefaultsIfNotSet()
